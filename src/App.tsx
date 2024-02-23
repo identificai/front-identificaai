8863db50db003f0ec3faefe3c312dc7e081c60a7
samuelferreira25 <samuel.ferreira2014.sf@gmail.com>
2024-02-23 11:15:03 -0300

    rota login adc

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Sobre from './paginas/sobre/Sobre'
 import Home from './paginas/home/Home'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Cadastro from './paginas/cadastro/Cadastro'
+import Login from './paginas/login/Login'
 function App() {
 
   return (
@@ -13,7 +14,9 @@ function App() {
       <Navbar></Navbar>
       <div className='min-h-[80vh]'>
         <Routes>
+       
           <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
           <Route path="/sobre" element={<Sobre />} />
           <Route path="/contato" element={<Contato />} />
           <Route path="/home" element={<Home />} />
