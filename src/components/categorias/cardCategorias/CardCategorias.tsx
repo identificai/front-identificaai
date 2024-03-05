33b07cf90a3299b2288111877e0d6dee06fe03d9
Luciana Rocha <lucianajrocha97@gmail.com>
2024-03-05 16:51:50 -0300

    cardCategoria atualizada

--- src/components/categorias/cardCategorias/CardCategorias.tsx
+++ src/components/categorias/cardCategorias/CardCategorias.tsx
@@ -7,20 +7,7 @@ interface CardCategoriaProps {
 
 function CardCategorias({categoria}: CardCategoriaProps) {
   return (
-    // <div className='border flex flex-col rounded-2xl overflow-hidden justify-between'>
-    //   <header className='py-2 px-6 bg-indigo-800 text-white font-bold text-2xl'>{categoria?.tipo}</header>
-    //   <p className='p-8 text-3xl bg-slate-200 h-full'>{categoria.descricao}</p>
-    //   <div className="flex">
-    //     <Link to={`/editarCategoria/${categoria.id}`} className='w-full text-slate-100 bg-indigo-400 hover:bg-indigo-800 flex items-center justify-center py-2'>
-    //       <button>Editar</button>
-    //     </Link>
-    //     <Link to={`/deletarCategoria/${categoria.id}`} className='text-slate-100 bg-red-400 hover:bg-red-700 w-full flex items-center justify-center'>
-    //       <button>Deletar</button>
-    //     </Link>
-    //   </div>
-    // </div>
-
-    <div className='relative flex w-96 flex-col rounded-xl bg-white bg-opacity-50 transition duration-300 hover:text-teal-400 bg-clip-border text-gray-700 shadow-md mt-[-75px]'>
+        <div className='relative flex w-96 flex-col rounded-xl bg-white bg-opacity-50 transition duration-300 hover:text-teal-400 bg-clip-border text-gray-700 shadow-md md-[-75px]'>
     <div>
       <div className='content-between'>
       <div className='p-4 '>
@@ -34,10 +21,10 @@ function CardCategorias({categoria}: CardCategoriaProps) {
            {categoria.descricao}
         </p>
     <div className="flex items-end justify-around">
-    <Link to={`/editarServicos/${categoria.id}`} className='w-5/12 text-white bg-green-400 hover:bg-green-800 flex items-center justify-center py-2 rounded-xl'>
+    <Link to={`/editarCategoria/${categoria.id}`} className='w-5/12 text-white bg-green-400 hover:bg-green-800 flex items-center justify-center py-2 rounded-xl'>
         <button>Editar</button>
       </Link>
-      <Link to={`/deletarServicos/${categoria.id}`} className='text-white bg-red-400 hover:bg-red-700 w-5/12 flex items-center justify-center py-2 rounded-xl'>
+      <Link to={`/deletarCategoria/${categoria.id}`} className='text-white bg-red-400 hover:bg-red-700 w-5/12 flex items-center justify-center py-2 rounded-xl'>
         <button>Deletar</button>
       </Link>
       </div>
