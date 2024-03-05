3fd687fb3c4ff9401d96a6449fd977ee4440c571
rafa v <r_venancio@protonmail.com>
2024-03-05 17:13:53 -0300

    Deletar serviços e categorias atualizado

--- src/components/servicos/deletarServicos/DeletarServicos.tsx
+++ src/components/servicos/deletarServicos/DeletarServicos.tsx
@@ -69,14 +69,14 @@ function DeletarServico() {
       <p className='text-center font-semibold mb-4'>Você tem certeza de que deseja apagar o serviço a seguir?</p>
 
       <div className='border flex flex-col rounded-2xl overflow-hidden justify-between'>
-        <header className='py-2 px-6 bg-indigo-600 text-white font-bold text-2xl'>Serviço</header>
+        <header className='py-2 px-6 bg-sky-400 text-white font-bold text-2xl'>Serviço</header>
         <div className="p-4">
           <p className='text-xl h-full'>{servico.nome}</p>
           <p>{servico.descricao}</p>
         </div>
         <div className="flex">
           <button className='text-slate-100 bg-red-400 hover:bg-red-600 w-full py-2' onClick={retornar}>Não</button>
-          <button className='w-full text-slate-100 bg-indigo-400 hover:bg-indigo-600 flex items-center justify-center' onClick={deletarServico}>
+          <button className='w-full text-slate-100 bg-green-400 hover:bg-green-800 flex items-center justify-center' onClick={deletarServico}>
             Sim
           </button>
         </div>
