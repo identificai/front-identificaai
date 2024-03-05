837e3754504f67f2893f9279163fec3afee0a3b2
MatheusLib <matheus.alexandre.libanio@gmail.com>
2024-03-05 16:27:06 -0300

    ajustando p/ deploys

--- src/components/categorias/modalCategorias/ModalCategorias.tsx
+++ src/components/categorias/modalCategorias/ModalCategorias.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import FormularioCategorias from '../formularioCategoria/FormularioCategoria';
 
 import 'reactjs-popup/dist/index.css';
--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import "./Home.css";
 import ModalServicos from "../../components/servicos/modalServicos/ModalServicos";
 import { Link } from "react-router-dom";
