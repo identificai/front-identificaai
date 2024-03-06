230bb2a1b252a0484a92f2977cb63f30cf16646b
MatheusLib <matheus.alexandre.libanio@gmail.com>
2024-03-06 09:00:42 -0300

    Home footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -7,6 +7,11 @@ function Footer() {
       <section className="bg-slate-700 py-16 "> 
         <div className="max-w-screen-xl px-4 mx-auto space-y-8 overflow-hidden sm:px-6 lg:px-8"> 
           <nav className="flex flex-wrap justify-center -mx-5 -my-2">
+          <div className="px-5 py-2 ">
+              <Link to="/" className="text-base leading-6 text-white block md:px-4 transition duration-300 hover:text-teal-400">
+                Home
+              </Link>
+            </div>
             <div className="px-5 py-2 ">
               <Link to="/sobre" className="text-base leading-6 text-white block md:px-4 transition duration-300 hover:text-teal-400">
                 Sobre
