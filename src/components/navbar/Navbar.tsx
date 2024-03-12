449ae2bddd2141adf065d068d4d09f6087fd5749
MatheusLib <matheus.alexandre.libanio@gmail.com>
2024-03-12 09:52:48 -0300

    melhorando navbar.2

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,113 +1,373 @@
-import { useContext } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import { AuthContext } from '../../contexts/AuthContext';
+import { useContext, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { AuthContext } from "../../contexts/AuthContext";
 import { toastAlerta } from "../../utils/toastAlerta";
-import newLogo from '../../assets/iden.png'
+import newLogo from "../../assets/iden.png";
+import useClickOutside from "../../eventListener/useClickOutside";
 
 function Navbar() {
   let navigate = useNavigate();
   const { usuario, handleLogout } = useContext(AuthContext);
 
+  const [userDropdown, setUserDropdown] = useState(false);
+
+  const ref = useClickOutside(handleClickOutside);
+
+  function handleClickOutside() {
+    setUserDropdown(false);
+  }
+
   function logout() {
     handleLogout();
-    toastAlerta('Usuário deslogado com sucesso', '');
-    navigate('/login');
+    toastAlerta("Usuário deslogado com sucesso", "");
+    navigate("/login");
   }
 
   let navbarComponent;
 
-  if (usuario && usuario.token !== "" ) {
+  if (usuario && usuario.token !== "") {
     navbarComponent = (
-<>
-<div
-  className="flex flex-col items-center justify-center mt-32"
-  x-cloak=""
-  x-data="appData()"
-  x-init="appInit()"
->
-  <div className="flex flex-col">
-    {/* Page Scroll Progress */}
-    <div
-      className="fixed inset-x-0 top-0 z-50 h-0.5 mt-0.5
+      <>
+        <div
+          className="flex flex-col items-center justify-center mt-32"
+          x-cloak=""
+          x-data="appData()"
+          x-init="appInit()"
+        >
+          <div className="flex flex-col">
+            {/* Page Scroll Progress */}
+            <div
+              className="fixed inset-x-0 top-0 z-50 h-0.5 mt-0.5
       bg-blue-500"
-    />
-    {/* Navbar */}
-    <nav
-      className="flex justify-around py-4 bg-white/80
+            />
+            {/* Navbar */}
+            <nav
+              className="flex justify-around py-4 bg-white/80
       backdrop-blur-md shadow-md w-full
       fixed top-0 left-0 right-0 z-10"
-    >
-      {/* Logo Container */}
-      <div className="flex items-center">
-        {/* Logo */}
-        <a className="cursor-pointer">
-          <h3 className="text-2xl font-medium text-cyan-700 hover:text-cyan-500">
-            <img
-              className="h-20 object-cover"
-              src={newLogo}
-              alt="Store Logo"
-            />
-          </h3>
-        </a>
-      </div>
-      {/* Links Section */}
-      <div className="items-center hidden space-x-8 lg:flex">
-        <a
-          className="flex text-cyan-700 hover:text-cyan-500
+            >
+              {/* Logo Container */}
+              <div className="flex items-center">
+                {/* Logo */}
+                <a className="cursor-pointer">
+                  <h3 className="text-2xl font-medium text-cyan-700 hover:text-cyan-500">
+                    <img
+                      className="h-20 object-cover"
+                      src={newLogo}
+                      alt="Store Logo"
+                    />
+                  </h3>
+                </a>
+              </div>
+              {/* Links Section */}
+              <div className="items-center hidden space-x-8 lg:flex">
+                <a
+                  className="flex text-cyan-700 hover:text-cyan-500
               cursor-pointer transition-colors duration-300  text-2xl font-semibold"
-        >
-          Sobre Nós
-        </a>
-      </div>
-      {/* Icon Menu Section */}
-      <div className="flex items-center space-x-5">
+                >
+                  Sobre Nós
+                </a>
+              </div>
+              {/* Icon Menu Section
         {/* Register */}
-        <a
-          className="flex text-cyan-700 hover:text-cyan-500
-              cursor-pointer transition-colors duration-300"
-        >
-          <svg
-            className="fill-current h-10 w-10 mr-2 mt-0.5"
-            xmlns="http://www.w3.org/2000/svg"
-            xmlnsXlink="http://www.w3.org/1999/xlink"
-            version="1.1"
-            width={24}
-            height={24}
-            viewBox="0 0 24 24"
-          >
-            <path d="M12 0L11.34 .03L15.15 3.84L16.5 2.5C19.75 4.07 22.09 7.24 22.45 11H23.95C23.44 4.84 18.29 0 12 0M12 4C10.07 4 8.5 5.57 8.5 7.5C8.5 9.43 10.07 11 12 11C13.93 11 15.5 9.43 15.5 7.5C15.5 5.57 13.93 4 12 4M12 6C12.83 6 13.5 6.67 13.5 7.5C13.5 8.33 12.83 9 12 9C11.17 9 10.5 8.33 10.5 7.5C10.5 6.67 11.17 6 12 6M.05 13C.56 19.16 5.71 24 12 24L12.66 23.97L8.85 20.16L7.5 21.5C4.25 19.94 1.91 16.76 1.55 13H.05M12 13C8.13 13 5 14.57 5 16.5V18H19V16.5C19 14.57 15.87 13 12 13M12 15C14.11 15 15.61 15.53 16.39 16H7.61C8.39 15.53 9.89 15 12 15Z" />
-          </svg>
-        </a>
-        {/* Login */}
-        {/* <a
-          className="flex text-gray-600 
-              cursor-pointer transition-colors duration-300
-              font-semibold text-blue-600"
-        >
-          <svg
-            className="fill-current h-5 w-5 mr-2 mt-0.5"
-            xmlns="http://www.w3.org/2000/svg"
-            xmlnsXlink="http://www.w3.org/1999/xlink"
-            version="1.1"
-            width={24}
-            height={24}
-            viewBox="0 0 24 24"
-          >
-            <path d="M10,17V14H3V10H10V7L15,12L10,17M10,2H19A2,2 0 0,1 21,4V20A2,2 0 0,1 19,22H10A2,2 0 0,1 8,20V18H10V20H19V4H10V6H8V4A2,2 0 0,1 10,2Z" />
-          </svg>
-          Login
-        </a> */}
-      </div>
-    </nav>
-  </div>
-</div>
-</>
+              <li
+                onClick={() => {
+                  setUserDropdown(true);
+                }}
+                id="userDropdown"
+                className="flex text-cyan-700 hover:text-cyan-500
+              cursor-pointer transition-colors duration-300 items-center space-x-5"
+              >
+                <div className="hover:bg-gray-200 rounded-md transition-all p-2">
+                  <div className="h-10">
+                    <svg
+                      className="fill-current h-10 w-10 mr-2 mt-0.5"
+                      xmlns="http://www.w3.org/2000/svg"
+                      xmlnsXlink="http://www.w3.org/1999/xlink"
+                      version="1.1"
+                      width={24}
+                      height={24}
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M12 0L11.34 .03L15.15 3.84L16.5 2.5C19.75 4.07 22.09 7.24 22.45 11H23.95C23.44 4.84 18.29 0 12 0M12 4C10.07 4 8.5 5.57 8.5 7.5C8.5 9.43 10.07 11 12 11C13.93 11 15.5 9.43 15.5 7.5C15.5 5.57 13.93 4 12 4M12 6C12.83 6 13.5 6.67 13.5 7.5C13.5 8.33 12.83 9 12 9C11.17 9 10.5 8.33 10.5 7.5C10.5 6.67 11.17 6 12 6M.05 13C.56 19.16 5.71 24 12 24L12.66 23.97L8.85 20.16L7.5 21.5C4.25 19.94 1.91 16.76 1.55 13H.05M12 13C8.13 13 5 14.57 5 16.5V18H19V16.5C19 14.57 15.87 13 12 13M12 15C14.11 15 15.61 15.53 16.39 16H7.61C8.39 15.53 9.89 15 12 15Z" />
+                    </svg>
+                  </div>
+                </div>
+                {usuario.token !== "" ? (
+                  <div
+                    id="dropdownDivider"
+                    className={`z-10 ${
+                      userDropdown ? "" : "hidden"
+                    } bg-white divide-y divide-gray-100 rounded-b-md shadow w-full md:w-60 absolute top-28 right-8 `}
+                  >
+                    <svg
+                      className="hidden md:block absolute text-white h-4 right-[7.3rem] -top-4 rotate-180 drop-shadow-2xl"
+                      x="0px"
+                      y="0px"
+                      viewBox="0 0 255 255"
+                      xmlSpace="preserve"
+                    >
+                      <polygon
+                        className="fill-gray-300"
+                        points="0,0 127.5,127.5 255,0"
+                      />
+                    </svg>
+                    <ul
+                      ref={ref}
+                      className="py-2 text-sm text-gray-700"
+                      aria-labelledby="dropdownDividerButton"
+                    >
+                      <li>
+                        <div className="block px-4 py-2">
+                          Olá, {usuario.nome}
+                        </div>
+                      </li>
+                      <li>
+                        <Link
+                          to={"/conta"}
+                          className="block px-4 py-2 hover:bg-gray-100"
+                        >
+                          Configurações
+                        </Link>
+                      </li>
+                      <li>
+                        <Link
+                          to={"/conta"}
+                          className="block px-4 py-2 hover:bg-gray-100"
+                        >
+                          Configurações
+                        </Link>
+                      </li>
+                    </ul>
+                    <div className="py-2">
+                      <button
+                        className="w-full text-left block px-4 py-2 hover:bg-gray-100 text-sm text-gray-700"
+                        onClick={handleLogout}
+                      >
+                        Sair
+                      </button>
+                    </div>
+                  </div>
+                ) : (
+                  <div
+                    id="dropdownDivider"
+                    className={`z-10 ${
+                      userDropdown ? "" : "hidden"
+                    } bg-white divide-y divide-gray-100 rounded-b-md shadow w-full md:w-60 absolute top-14 right-0 md:-right-24`}
+                  >
+                    <svg
+                      className="hidden md:block absolute text-white h-4 right-[7.3rem] -top-4 rotate-180 drop-shadow-2xl"
+                      x="0px"
+                      y="0px"
+                      viewBox="0 0 255 255"
+                      xmlSpace="preserve"
+                    >
+                      <polygon
+                        className="fill-gray-300"
+                        points="0,0 127.5,127.5 255,0"
+                      />
+                    </svg>
+                    <ul
+                      ref={ref}
+                      className="py-2 text-sm text-gray-700"
+                      aria-labelledby="dropdownDividerButton"
+                    >
+                      <li>
+                        <Link
+                          to={"/login"}
+                          className="block px-4 py-2 hover:bg-gray-100"
+                        >
+                          Entrar
+                        </Link>
+                      </li>
+                      <li>
+                        <Link
+                          to={"/cadastro"}
+                          className="block px-4 py-2 hover:bg-gray-100"
+                        >
+                          Criar conta
+                        </Link>
+                      </li>
+                    </ul>
+                  </div>
+                )}
+              </li>
+            </nav>
+          </div>
+        </div>
+      </>
     );
   } else {
     navbarComponent = (
       <>
-        {/* Conteúdo do navbar para usuários não logados */}
-      </>
+      <div
+        className="flex flex-col items-center justify-center mt-32"
+        x-cloak=""
+        x-data="appData()"
+        x-init="appInit()"
+      >
+        <div className="flex flex-col">
+          {/* Page Scroll Progress */}
+          <div
+            className="fixed inset-x-0 top-0 z-50 h-0.5 mt-0.5
+    bg-blue-500"
+          />
+          {/* Navbar */}
+          <nav
+            className="flex justify-around py-4 bg-white/80
+    backdrop-blur-md shadow-md w-full
+    fixed top-0 left-0 right-0 z-10"
+          >
+            {/* Logo Container */}
+            <div className="flex items-center">
+              {/* Logo */}
+              <a className="cursor-pointer">
+                <h3 className="text-2xl font-medium text-cyan-700 hover:text-cyan-500">
+                  <img
+                    className="h-20 object-cover"
+                    src={newLogo}
+                    alt="Store Logo"
+                  />
+                </h3>
+              </a>
+            </div>
+            {/* Links Section */}
+            <div className="items-center hidden space-x-8 lg:flex">
+              <a
+                className="flex text-cyan-700 hover:text-cyan-500
+            cursor-pointer transition-colors duration-300  text-2xl font-semibold"
+              >
+                Sobre Nós
+              </a>
+            </div>
+            {/* Icon Menu Section
+      {/* Register */}
+            <li
+              onClick={() => {
+                setUserDropdown(true);
+              }}
+              id="userDropdown"
+              className="flex text-cyan-700 hover:text-cyan-500
+            cursor-pointer transition-colors duration-300 items-center space-x-5"
+            >
+              <div className="hover:bg-gray-200 rounded-md transition-all p-2">
+                <div className="h-10">
+                  <svg
+                    className="fill-current h-10 w-10 mr-2 mt-0.5"
+                    xmlns="http://www.w3.org/2000/svg"
+                    xmlnsXlink="http://www.w3.org/1999/xlink"
+                    version="1.1"
+                    width={24}
+                    height={24}
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M12 0L11.34 .03L15.15 3.84L16.5 2.5C19.75 4.07 22.09 7.24 22.45 11H23.95C23.44 4.84 18.29 0 12 0M12 4C10.07 4 8.5 5.57 8.5 7.5C8.5 9.43 10.07 11 12 11C13.93 11 15.5 9.43 15.5 7.5C15.5 5.57 13.93 4 12 4M12 6C12.83 6 13.5 6.67 13.5 7.5C13.5 8.33 12.83 9 12 9C11.17 9 10.5 8.33 10.5 7.5C10.5 6.67 11.17 6 12 6M.05 13C.56 19.16 5.71 24 12 24L12.66 23.97L8.85 20.16L7.5 21.5C4.25 19.94 1.91 16.76 1.55 13H.05M12 13C8.13 13 5 14.57 5 16.5V18H19V16.5C19 14.57 15.87 13 12 13M12 15C14.11 15 15.61 15.53 16.39 16H7.61C8.39 15.53 9.89 15 12 15Z" />
+                  </svg>
+                </div>
+              </div>
+              {usuario.token !== "" ? (
+                <div
+                  id="dropdownDivider"
+                  className={`z-10 ${
+                    userDropdown ? "" : "hidden"
+                  } bg-white divide-y divide-gray-100 rounded-b-md shadow w-full md:w-60 absolute top-28 right-8 `}
+                >
+                  <svg
+                    className="hidden md:block absolute text-white h-4 right-[7.3rem] -top-4 rotate-180 drop-shadow-2xl"
+                    x="0px"
+                    y="0px"
+                    viewBox="0 0 255 255"
+                    xmlSpace="preserve"
+                  >
+                    <polygon
+                      className="fill-gray-300"
+                      points="0,0 127.5,127.5 255,0"
+                    />
+                  </svg>
+                  <ul
+                    ref={ref}
+                    className="py-2 text-sm text-gray-700"
+                    aria-labelledby="dropdownDividerButton"
+                  >
+                    <li>
+                      <div className="block px-4 py-2">
+                        Olá, {usuario.nome}
+                      </div>
+                    </li>
+                    <li>
+                      <Link
+                        to={"/conta"}
+                        className="block px-4 py-2 hover:bg-gray-100"
+                      >
+                        Configurações
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        to={"/conta"}
+                        className="block px-4 py-2 hover:bg-gray-100"
+                      >
+                        Configurações
+                      </Link>
+                    </li>
+                  </ul>
+                  <div className="py-2">
+                    <button
+                      className="w-full text-left block px-4 py-2 hover:bg-gray-100 text-sm text-gray-700"
+                      onClick={handleLogout}
+                    >
+                      Sair
+                    </button>
+                  </div>
+                </div>
+              ) : (
+                <div
+                  id="dropdownDivider"
+                  className={`z-10 ${
+                    userDropdown ? "" : "hidden"
+                  } bg-white divide-y divide-gray-100 rounded-b-md shadow w-full md:w-60 absolute top-28 right-8`}
+                >
+                  <svg
+                    className="hidden md:block absolute text-white h-4 right-[7.3rem] -top-4 rotate-180 drop-shadow-2xl"
+                    x="0px"
+                    y="0px"
+                    viewBox="0 0 255 255"
+                    xmlSpace="preserve"
+                  >
+                    <polygon
+                      className="fill-gray-300"
+                      points="0,0 127.5,127.5 255,0"
+                    />
+                  </svg>
+                  <ul
+                    ref={ref}
+                    className="py-2 text-sm text-gray-700"
+                    aria-labelledby="dropdownDividerButton"
+                  >
+                    <li>
+                      <Link
+                        to={"/login"}
+                        className="block px-4 py-2 hover:bg-gray-100"
+                      >
+                        Entrar
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        to={"/cadastro"}
+                        className="block px-4 py-2 hover:bg-gray-100"
+                      >
+                        Criar conta
+                      </Link>
+                    </li>
+                  </ul>
+                </div>
+              )}
+            </li>
+          </nav>
+        </div>
+      </div>
+    </>
     );
   }
 
