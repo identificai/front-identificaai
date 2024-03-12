d09cac56bd37a6cb2bf2bab136df61601f433841
MatheusLib <matheus.alexandre.libanio@gmail.com>
2024-03-12 13:08:15 -0300

    atualizações página sobre

--- src/paginas/sobre/Sobre.tsx
+++ src/paginas/sobre/Sobre.tsx
@@ -1,87 +1,106 @@
-import './Sobre.css';
-import imagemSobre from "../../assets/sobrenos.jpg"
+import "./Sobre.css";
+import imagemSobre from "../../assets/sobrenos.jpg";
+import Introducao from "../../components/introducao/Introducao";
 
 function Sobre() {
-    return (
-
-      <>
-  <section className="relative pt-16 bg-blueGray-50">
-    <div className="container mx-auto">
-      <div className="flex flex-wrap items-center">
-        <div className="w-10/12 md:w-6/12 lg:w-4/12 px-12 md:px-4 mr-auto ml-auto -mt-78">
-          <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded-lg bg-pink-500">
-            <img
-              alt="..."
-              src= {imagemSobre}
-              className="w-full align-middle rounded-t-lg"
-            />
-            <blockquote className="relative p-8 mb-4">
-              <h4 className="text-xl font-bold text-black">
-              Sobre nossa organização
-              </h4>
-              <p className="mb-4 text-blueGray-500">
-              Nossa visão é criar um mundo onde cada pessoa tenha uma identidade legal reconhecida e respeitada, independentemente de sua origem, status socioeconômico ou local de residência. Queremos ser líderes na prestação de serviços que ajudem a eliminar as barreiras burocráticas e a promover a inclusão e a igualdade de oportunidades para todos.              </p>
-            </blockquote>
-          </div>
-        </div>
-        <div className="w-full md:w-6/12 px-4">
-          <div className="flex flex-wrap">
-            <div className="w-full md:w-6/12 px-4">
-              <div className="relative flex flex-col mt-4">
-                <div className="px-4 py-5 flex-auto">
-                  <div className="text-blueGray-500 p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-white">
-                    <i className="fas fa-sitemap" />
-                  </div>
-                  <h6 className="text-xl mb-1 font-semibold">Acessibilidade</h6>
+  return (
+    <>
+      <Introducao />
+      <section className="relative pt-16 bg-blueGray-50">
+        <div className="container mx-auto">
+        <h2 className="text-2xl text-gray-900 font-bold md:text-4xl text-center">
+              Nossos Valores
+            </h2>
+          <div className="flex flex-wrap items-center">    
+            <div className="w-10/12 md:w-6/12 lg:w-4/12 px-12 md:px-4 mr-auto ml-auto -mt-78">
+              <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded-lg bg-pink-500">
+                <img
+                  alt="..."
+                  src={imagemSobre}
+                  className="w-full align-middle rounded-t-lg"
+                />
+                <blockquote className="relative p-8 mb-4">
+                  <h4 className="text-xl font-bold text-black">
+                    Sobre nossa organização
+                  </h4>
                   <p className="mb-4 text-blueGray-500">
-                  Buscamos tornar nossos serviços acessíveis a todas as pessoas, garantindo que os custos não sejam uma barreira para obter uma identidade legal.
+                    Nossa visão é criar um mundo onde cada pessoa tenha uma
+                    identidade legal reconhecida e respeitada, independentemente
+                    de sua origem, status socioeconômico ou local de residência.
+                    Queremos ser líderes na prestação de serviços que ajudem a
+                    eliminar as barreiras burocráticas e a promover a inclusão e
+                    a igualdade de oportunidades para todos.{" "}
                   </p>
-                </div>
-              </div>
-              <div className="relative flex flex-col min-w-0">
-                <div className="px-4 py-5 flex-auto">
-                  <div className="text-blueGray-500 p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-white">
-                    <i className="fas fa-drafting-compass" />
-                  </div>
-                  <h6 className="text-xl mb-1 font-semibold">
-                  Inovação
-                  </h6>
-                  <p className="mb-4 text-blueGray-500">
-                  Estamos sempre buscando maneiras de melhorar e simplificar nossos processos, utilizando tecnologia e práticas eficientes para oferecer uma experiência superior aos nossos clientes.
-                  </p>
-                </div>
+                </blockquote>
               </div>
             </div>
             <div className="w-full md:w-6/12 px-4">
-              <div className="relative flex flex-col min-w-0 mt-4">
-                <div className="px-4 py-5 flex-auto">
-                  <div className="text-blueGray-500 p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-white">
-                    <i className="fas fa-newspaper" />
+              <div className="flex flex-wrap">
+                <div className="w-full md:w-6/12 px-4">
+                  <div className="relative flex flex-col mt-4">
+                    <div className="px-4 py-5 flex-auto">
+                      <div className="text-blueGray-500 p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-white">
+                        <i className="fas fa-sitemap" />
+                      </div>
+                      <h6 className="text-xl mb-1 font-semibold">
+                        Acessibilidade
+                      </h6>
+                      <p className="mb-4 text-blueGray-500">
+                        Buscamos tornar nossos serviços acessíveis a todas as
+                        pessoas, garantindo que os custos não sejam uma barreira
+                        para obter uma identidade legal.
+                      </p>
+                    </div>
                   </div>
-                  <h6 className="text-xl mb-1 font-semibold">Qualidade</h6>
-                  <p className="mb-4 text-blueGray-500">
-                  Comprometemo-nos com a excelência em todos os aspectos de nossos serviços, garantindo a precisão e a confiabilidade de todos os documentos fornecidos.
-                  </p>
+                  <div className="relative flex flex-col min-w-0">
+                    <div className="px-4 py-5 flex-auto">
+                      <div className="text-blueGray-500 p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-white">
+                        <i className="fas fa-drafting-compass" />
+                      </div>
+                      <h6 className="text-xl mb-1 font-semibold">Inovação</h6>
+                      <p className="mb-4 text-blueGray-500">
+                        Estamos sempre buscando maneiras de melhorar e
+                        simplificar nossos processos, utilizando tecnologia e
+                        práticas eficientes para oferecer uma experiência
+                        superior aos nossos clientes.
+                      </p>
+                    </div>
+                  </div>
                 </div>
-              </div>
-              <div className="relative flex flex-col min-w-0">
-                <div className="px-4 py-5 flex-auto">
-                  <div className="text-blueGray-500 p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-white">
-                    <i className="fas fa-file-alt" />
+                <div className="w-full md:w-6/12 px-4">
+                  <div className="relative flex flex-col min-w-0 mt-4">
+                    <div className="px-4 py-5 flex-auto">
+                      <div className="text-blueGray-500 p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-white">
+                        <i className="fas fa-newspaper" />
+                      </div>
+                      <h6 className="text-xl mb-1 font-semibold">Qualidade</h6>
+                      <p className="mb-4 text-blueGray-500">
+                        Comprometemo-nos com a excelência em todos os aspectos
+                        de nossos serviços, garantindo a precisão e a
+                        confiabilidade de todos os documentos fornecidos.
+                      </p>
+                    </div>
                   </div>
-                  <h6 className="text-xl mb-1 font-semibold">Integração</h6>
-                  <p className="mb-4 text-blueGray-500">
-                  Atuamos com honestidade e transparência em todas as nossas interações, mantendo os mais altos padrões éticos em nosso trabalho.
-                  </p>
+                  <div className="relative flex flex-col min-w-0">
+                    <div className="px-4 py-5 flex-auto">
+                      <div className="text-blueGray-500 p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-white">
+                        <i className="fas fa-file-alt" />
+                      </div>
+                      <h6 className="text-xl mb-1 font-semibold">Integração</h6>
+                      <p className="mb-4 text-blueGray-500">
+                        Atuamos com honestidade e transparência em todas as
+                        nossas interações, mantendo os mais altos padrões éticos
+                        em nosso trabalho.
+                      </p>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    </div>
-  </section>
-    {/* /*    <div className='background-sobre'>
+      </section>
+      {/* /*    <div className='background-sobre'>
        
   
             <div className="flex flex-col gap-7 items-center justify-center py-9">
@@ -115,10 +134,8 @@ function Sobre() {
           </div>
 
     </div>*/}
-       
-       
-      </> 
-    );
+    </>
+  );
 }
 
 export default Sobre;
